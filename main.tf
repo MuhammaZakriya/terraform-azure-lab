5a33a34a95735afd8db0e02a7e9ee68322533843
Muhammad Zakriya <zakriyamalik852@gmail.com>
2026-04-02 21:51:29 +0500

    Second subnet added(Resolved errors)

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "azurerm_network_interface" "nic" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
+    subnet_id                     = azurerm_subnet.subnet1.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
@@ -131,6 +131,6 @@ resource "azurerm_network_security_group" "nsg" {
 
 # Associate NSG with Subnet
 resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
-  subnet_id                 = azurerm_subnet.subnet.id
+  subnet_id                 = azurerm_subnet.subnet1.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
