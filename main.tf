fcdd1227625de12f24bbb16557d4416ac6840abe
Muhammad Zakriya <zakriyamalik852@gmail.com>
2026-04-02 23:45:21 +0500

    Added second VM(Private)

--- main.tf
+++ main.tf
@@ -153,3 +153,27 @@ resource "azurerm_subnet_route_table_association" "public_rt_assoc" {
   route_table_id = azurerm_route_table.public_rt.id
 }
 
+# Private VM
+resource "azurerm_linux_virtual_machine" "private_vm" {
+  name                = "${var.resource_group_name}-private-vm"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  size                = "Standard_B2ps_v2"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  disable_password_authentication = false
+
+  network_interface_ids = [azurerm_network_interface.private_nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-arm64"
+    version   = "latest"
+  }
+}
