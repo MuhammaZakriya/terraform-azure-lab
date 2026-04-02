30116ed0cd3f545973ee55bb7b5c6c38ea0c79f5
Muhammad Zakriya <zakriyamalik852@gmail.com>
2026-04-02 22:00:30 +0500

    Second subnet added(Resolved errors(1 NIC issue))

--- main.tf
+++ main.tf
@@ -50,18 +50,7 @@ resource "azurerm_subnet" "subnet2" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "${var.resource_group_name}-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet1.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip.id
-  }
-}
 
 
 resource "azurerm_virtual_machine" "vm" {
