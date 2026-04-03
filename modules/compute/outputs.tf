610287ea85e2a95e5e9d42c1b86f4a2056326252
Muhammad Zakriya <zakriyamalik852@gmail.com>
2026-04-03 19:47:19 +0500

    Structured with modules(3)

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -1,2 +1,2 @@
-﻿output "public_vm_id" { value = azurerm_linux_virtual_machine.public_vm.id }
+﻿output "public_vm_id" { value = azurerm_virtual_machine.public_vm.id }
 output "private_vm_id" { value = azurerm_linux_virtual_machine.private_vm.id }
